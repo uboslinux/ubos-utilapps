e89ad766a0cfbb14685b055c35dfc008ffb083d5
Johannes Ernst <jernst@indiecomputing.com>
2018-01-13 05:13:59 +0000

    Cleaning up license comments Now licensed using AGPL3

--- redirect/tests/RedirectTest2.pm
+++ redirect/tests/RedirectTest2.pm
@@ -2,21 +2,7 @@
 #
 # Simple test for redirect, with default
 #
-# This file is part of redirect.
-# (C) 2012-2015 Indie Computing Corp.
-#
-# gladiwashere is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# gladiwashere is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with gladiwashere.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2014 and later, Indie Computing Corp. All rights reserved. License: see package.
 #
 
 use strict;
